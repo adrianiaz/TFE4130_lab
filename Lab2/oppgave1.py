a52475b5046df53cf85c0115418d69268f472a4b
Adrian Niazi <adrian.izi@protonmail.com>
2024-02-22 11:18:42 +0100

    py function for cross-correlation

--- Lab2/oppgave1.py
+++ Lab2/oppgave1.py
@@ -7,23 +7,24 @@ fs =  1000
 n = 10
 
 #array for n sampler for lydsignal som mottat fra henholdsvis mic 1 & 2
-x1 = np.zeros(n)
-x2 = np.zeros(n)
+x = np.zeros(n)
+y = np.zeros(n)
 
 #funksjon som tar inn to array x1 og x2 og samplingfrekvens, og gir ut tidsforsinkelsen i sekund.
-def tidsforsinkelse(x1,x2,fs):
+def forsinkelse(x,y,fs):
     #kryskorrelasjon mellom x1 og x2, tar absoluttverdi for di vi kun er interessert i positive verdier
     #Vi får her at len(crosscorr) = len(x1) + len(x2) - 1
-    crosscorr = np.abs(np.correlate(x1, x2))
+    crosscorr = np.abs(np.correlate(x, y), "full")
 
     #Finner indeksen i krysskorrelasjonenslisten som
     max_index_crosscorr = np.maxarg(crosscorr)
 
     #finner den sampelforsinkelsen, altså den l-en, hvor signalene er like, altså "sampelforsinkelsen"
-    sampelforsinkelse = max_index_crosscorr - (len(x1)- 1)
+    sampelforsinkelse = max_index_crosscorr - (len(y)- 1)
+
 
     #tidsforsinkelsen blir da fra teorien
     tidsforsinkelse = sampelforsinkelse/fs
 
-    return tidsforsinkelse
+    return sampelforsinkelse, tidsforsinkelse
 
